755f3be9db503cb850e04cf470383971661cfa7f
YoungLemon1 <yanivxyz@gmail.com>
2023-03-15 12:28:32 +0200

    first app creation

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">EShop</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
