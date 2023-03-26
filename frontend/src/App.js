998ed41a2f668257aa5d7a59f6228496f0597087
yaniv <yaniv@DESKTOP-K7C29F9>
2023-03-26 13:25:14 +0300

    removed use-reducer-logger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import NavBar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage.js';
-import ProductPage from './pages/ProductPage.js';
+import "./App.css";
+import NavBar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage.js";
+import ProductPage from "./pages/ProductPage.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        
-        <header >
-        <NavBar bg='dark' variant="dark">
-          <Container>
-            <LinkContainer></LinkContainer>
-            <NavBar.Brand>
-              ESHOP
-            </NavBar.Brand>
-          </Container>
+        <header>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>ESHOP</NavBar.Brand>
+              </LinkContainer>
+            </Container>
           </NavBar>
         </header>
         <main>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,47 +1,52 @@
-import { Link } from 'react-router-dom';
-import data from '../data.js';
-import { useEffect, useReducer, useState } from 'react';
-import axios from 'axios';
-import logger from 'use-reducer-logger';
+import { Link } from "react-router-dom";
+import data from "../data.js";
+import { useEffect, useReducer, useState } from "react";
+import axios from "axios";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'GET_REQUEST': return {...state, loading: true };
-    case 'GET_SUCCESS': return {...state,products: action.payload,loading: false};
-    case 'GET_FAIL': return {...state, loading: false, error: action.payload };
-    default: return state;
+    case "GET_REQUEST":
+      return { ...state, loading: true };
+    case "GET_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "GET_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
   }
-}
+};
 
 function HomePage() {
-
-  const [{ loading, error, products}, dispatch] = useReducer(reducer,{ loading: true, error: "", products: []});
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+    products: [],
+  });
 
   useEffect(() => {
-    const getProducts = async() => {
-      dispatch({ type: 'GET_REQUEST' });
-
-      try {
-        const res = await axios.get('http://localhost:5000/api/v1/products');
-        dispatch({ type: 'GET_SUCCESS', payload: res.data });
-      } catch (err) {
-        dispatch({type: 'GET_FAIL', payload: err.message});
-      }
-    } 
+    const getProducts = async () => {
+      // dispatch({ type: 'GET_REQUEST' });
+      // try {
+      //   const res = await axios.get('http://localhost:5000/api/v1/products');
+      //   dispatch({ type: 'GET_SUCCESS', payload: res.data });
+      // } catch (err) {
+      //   dispatch({type: 'GET_FAIL', payload: err.message});
+      // }
+    };
 
     getProducts();
-  },[])
+  }, []);
 
   return (
     <div>
       <h1>Products</h1>
       <div className="products">
-        {
-          loading ? (<p>Loading...</p>) 
-          : 
-          error ? (<p>{error}</p>)
-          :
-          (products.map((product) => (
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          products.map((product) => (
             <div key={product.token} className="product">
               <Link to={`/product/${product.token}`}>
                 <img alt={product.name} src={product.image} />
@@ -57,7 +62,8 @@ function HomePage() {
                 </Link>
               </div>
             </div>
-          )))}
+          ))
+        )}
       </div>
     </div>
   );
