2592b6037f915e5eb0ba3f07a1f32ad91dcd8f13
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-09 01:37:08 +0300

    "

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import Product from '../models/ProductModel.js';
-import data from '../data.js';
+import express from "express";
+import Product from "../models/ProductModel.js";
+import data from "../data.js";
 
 const seedRouter = express.Router();
 
-seedRouter.get('/', async (req, res) => {
-    await Product.deleteMany({});
-    const createdProducts = await Product.insertMany(data.products);
-    res.send({createdProducts});
-
-    // await Product.deleteMany({});
-    // const createdProducts = await Product.insertMany(data.products);
+seedRouter.get("/", async (req, res) => {
+  await Product.deleteMany({});
+  const createdProducts = await Product.insertMany(data.products);
+  res.send({ createdProducts });
 });
 
 export default seedRouter;
